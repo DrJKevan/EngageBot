2fb08a1a42859bbdb64ec8b1e722a53a0f187c5c
Jonathan Kevan <jkevan@dhcp-10-142-226-178.uawifi.arizona.edu>
2023-06-15 11:09:37 -0700

    remove API key into env

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import os
 from langchain.llms import OpenAI
+from dotenv import load_dotenv
 
-# Step 2: Set the OpenAI API Key
-os.environ["OPENAI_API_KEY"] = "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"
+load_dotenv()  # take environment variables from .env
 
-# Step 3: Initialize the OpenAI Class
+# Initialize the OpenAI Class
 llm = OpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"))
 
-# Step 4: Build a Language Model Application
+# Build a Language Model Application
 text = "What would be a good company name for a company that makes colorful socks?"
 response = llm(text)
 
