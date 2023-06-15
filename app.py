d3ab27773c8feef39415581ec0e0b70a5cb00df3
Jonathan Kevan <jkevan@dhcp-10-142-226-178.uawifi.arizona.edu>
2023-06-15 14:58:29 -0700

    Resolved issue with dotenv not working

--- app.py
+++ app.py
@@ -2,10 +2,17 @@ import os
 from langchain.llms import OpenAI
 from dotenv import load_dotenv
 
-load_dotenv()  # take environment variables from .env
+# Specify the path to the .env file
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+
+# Load the .env file
+load_dotenv(dotenv_path)
+
+api_key = os.getenv("OPENAI_API_KEY")
+print(f"API Key: {api_key}")  # print the API key to check if it's correctly loaded
 
 # Initialize the OpenAI Class
-llm = OpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"))
+llm = OpenAI(openai_api_key=api_key)
 
 # Build a Language Model Application
 text = "What would be a good company name for a company that makes colorful socks?"
